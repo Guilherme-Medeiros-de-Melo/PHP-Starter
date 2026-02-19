d7ebc19aa62a8e47a87a70b036b036dd282279d8
GuilhermeMM <guilhermemedeiros0demelo@gmail.com>
2026-02-19 16:20:25 -0300

    Added function example

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,5 +1,29 @@
 <?php
-echo 'My first PHP script! ';
+//I'm running this on FarManager, so HTML tags may not work
 
-echo phpversion();
+echo "My first PHP script!\n";
+
+echo phpversion(); 
+
+echo "\n";
+
+$name = "Test";
+$ages = 26 + 1;
+
+echo "This guy name is $name, age $ages \n";
+
+var_dump($ages);
+
+function ageUp($age){
+    echo "You're $age now, ";
+
+    $year = 5;
+    $age += $year;
+
+    echo "you'll be $age in $year years\n";
+
+    return $age;
+}
+
+ageUp($ages);
 ?>
